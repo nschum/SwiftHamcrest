a2e6e67b60a8dd615291cd57122fc7cb0f5914a1
Nikolaj Schumacher <git@nschum.de>
2014-09-10 18:04:33 +0200

    Add iOS demo project.

---
+++ HamcrestDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamcrestDemo
+//
+//  Created by Nikolaj Schumacher on 2014-09-10.
+//  Copyright (c) 2014 Nikolaj Schumacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HamcrestDemoTests/HamcrestDemoTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+import Hamcrest
+
+class HamcrestDemoTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        HamcrestReportFunction = {(message, file, line) in XCTFail(message, file:file, line:line)}
+    }
+
+    // Look at README.playground for a tutorial.
+
+    func testSuccess() {
+        let x = 10
+        assertThat(x, equalTo(10))
+    }
+
+    func testFailure() {
+        let x = -10
+        assertThat(x, equalTo(10))
+    }
+}
