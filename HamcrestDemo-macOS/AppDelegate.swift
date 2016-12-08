209f82a4b8ab86c285d4f6e739b07c3e9f999a8f
Nikolaj Schumacher <git@nschum.de>
2016-12-09 00:48:25 +0100

    Update demo projects to Swift 3

--- HamcrestDemo-macOS/AppDelegate.swift
+++ HamcrestDemo-macOS/AppDelegate.swift
@@ -13,11 +13,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
