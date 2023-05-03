0776d53d52de81e9c16a97146185c178a0c848b4
René Pirringer <rene@pirringer.at>
2023-05-03 08:11:37 +0200

    Renamed SwiftHamcrest to Hamcrest in the Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftHamcrest",
+    name: "Hamcrest",
     products: [
         .library(name: "SwiftHamcrest", targets: ["SwiftHamcrest"]),
     ],
