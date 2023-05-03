42306dea79f8bd4d5807af0f961938d455c080d7
René Pirringer <rene@pirringer.at>
2023-05-03 08:43:06 +0200

    Reverted last change.

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hamcrest",
+    name: "SwiftHamcrest",
     products: [
-        .library(name: "Hamcrest", targets: ["SwiftHamcrest"]),
+        .library(name: "SwiftHamcrest", targets: ["SwiftHamcrest"]),
     ],
     targets: [
         .target(name: "SwiftHamcrest", dependencies: [], path: "Hamcrest"),
