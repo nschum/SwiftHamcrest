ba30789b9b4630be814fb34d44fc3994b50b9732
René Pirringer <rene@pirringer.at>
2025-04-28 16:44:40 +0200

    renamed to enable

--- SwiftTesting/Source/Main/Reporter.swift
+++ SwiftTesting/Source/Main/Reporter.swift
@@ -8,7 +8,7 @@ import Foundation
 import Hamcrest
 import Testing
 
-public func enableSwiftTestingHamcrest() {
+public func enable() {
     Hamcrest.SwiftTestingHamcrestReportFunction = { message, fileID, file, line, column in
         let location = Testing.SourceLocation(fileID: fileID, filePath: "\(file)", line: Int(line), column: Int(column))
         Issue.record(Testing.Comment(rawValue: message), sourceLocation: location)
