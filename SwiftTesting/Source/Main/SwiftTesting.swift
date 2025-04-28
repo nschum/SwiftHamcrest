e874a74e6dc3aff01f010ae66785997f31458a3d
René Pirringer <rene@pirringer.at>
2025-04-28 16:03:46 +0200

    removed old checkMatcher and now the macro only uses public api

--- SwiftTesting/Source/Main/SwiftTesting.swift
+++ SwiftTesting/Source/Main/SwiftTesting.swift
@@ -10,25 +10,11 @@ import SwiftSyntax
 import SwiftSyntaxMacros
 import Testing
 
-@_disfavoredOverload public func checkMatcher<T>(
-    _ value: @autoclosure () throws -> T,
-    _ matcher: Matcher<T>,
-    comments: @autoclosure () -> [Comment],
-    isRequired: Bool,
-    sourceLocation: Testing.SourceLocation
-) -> Result<Void, any Error> {
-    if let message = applyMatcher(matcher, toValue: value) {
-        let expression = Testing.__Expression.__fromSyntaxNode(message)
-        return __checkValue(
-            false,
-            expression: expression,
-            expressionWithCapturedRuntimeValues: expression,
-            comments: comments(),
-            isRequired: isRequired,
-            sourceLocation: sourceLocation
-        )
+public func enable() {
+    Hamcrest.SwiftTestingHamcrestReportFunction = { message, fileID, file, line, column in
+        let location = Testing.SourceLocation(fileID: fileID, filePath: "\(file)", line: Int(line), column: Int(column))
+        Issue.record(Testing.Comment(rawValue: message), sourceLocation: location)
     }
-    return .success(())
 }
 
 @_disfavoredOverload public func __assertThat<T>(
@@ -43,7 +29,6 @@ import Testing
     }
 }
 
-
 @freestanding(expression) public macro assertThat<T>(
     _ value: @autoclosure () throws -> T,
     _ matcher: Matcher<T>,
@@ -51,9 +36,3 @@ import Testing
     sourceLocation: Testing.SourceLocation = #_sourceLocation
 ) = #externalMacro(module: "HamcrestSwiftTestingMacros", type: "AssertThatMacro")
 
-public func enable() {
-    Hamcrest.SwiftTestingHamcrestReportFunction = { message, fileID, file, line, column in
-        let location = Testing.SourceLocation(fileID: fileID, filePath: "\(file)", line: Int(line), column: Int(column))
-        Issue.record(Testing.Comment(rawValue: message), sourceLocation: location)
-    }
-}
