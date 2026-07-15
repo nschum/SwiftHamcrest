154bdbe70bf745ed0e678a310f13ef3d910db781
René Pirringer <rene@pirringer.at>
2026-07-15 16:26:39 +0200

    only add Issue.record on OS 26+

--- Hamcrest/Hamcrest.swift
+++ Hamcrest/Hamcrest.swift
@@ -21,10 +21,12 @@ nonisolated(unsafe) public let HamcrestDefaultReportFunction =
 
 
 func reporterFunction(_ message: String = "", fileID: String, file: StaticString, line: UInt, column: UInt) {
-    #if canImport(Testing)
+    if #available(macOS 26, iOS 26, tvOS 26, watchOS 26, *) {
+#if canImport(Testing)
         Issue.record("add 'import HamcrestSwiftTesting'", severity: .warning)
         Issue.record(Testing.Comment(rawValue: message), sourceLocation: Testing.SourceLocation(fileID: fileID, filePath: String(describing: file), line: Int(line), column: 0))
-    #endif
+#endif
+    }
     XCTFail(message, file: file, line: line)
 }
 
